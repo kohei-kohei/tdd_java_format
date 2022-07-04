ae6b702d7a89c0b333eef4b2a7fd92a1c0f515de
kohei-kohei <sky6sk212@gmail.com>
2022-07-04 14:31:00 +0900

    メソッドの引き上げ

--- src/main/java/com/tdd/money/Dollar.java
+++ src/main/java/com/tdd/money/Dollar.java
@@ -10,8 +10,4 @@ class Dollar extends Money{
     return new Dollar(amount * multiplier);
   }
 
-  public boolean equals(Object object) {
-    Money money = (Money) object;
-    return amount == money.amount;
-  }
 }
