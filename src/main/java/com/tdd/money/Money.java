6cc2cfb38a5b100614cbb6ef332c39cb109b2e68
kohei-kohei <sky6sk212@gmail.com>
2022-07-13 14:28:52 +0900

    テストを変更する

--- src/main/java/com/tdd/money/Money.java
+++ src/main/java/com/tdd/money/Money.java
@@ -1,6 +1,6 @@
 package com.tdd.money;
 
-public class Money {
+public class Money implements Expression {
   protected int amount;
   protected String currency;
 
@@ -13,7 +13,7 @@ public class Money {
     return new Money(amount * multiplier, currency);
   }
 
-  Money plus(Money addend) {
+  Expression plus(Money addend) {
     return new Money(amount + addend.amount, currency);
   }
 
