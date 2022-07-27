1eb5d3e8812e732cb031fabc3a3c86e81297121b
kohei-kohei <sky6sk212@gmail.com>
2022-07-27 14:29:56 +0900

    MoneyをExpressionにしていく

--- src/test/java/com/tdd/money/MoneyTest.java
+++ src/test/java/com/tdd/money/MoneyTest.java
@@ -70,7 +70,7 @@ public class MoneyTest {
   @Test
   public void testMixedAddition() {
     Money fiveBucks = Money.dollar(5);
-    Money tenFrans = Money.franc(10);
+    Expression tenFrans = Money.franc(10);
     Bank bank = new Bank();
     bank.addRate("CHF", "USD", 2);
     Money result = bank.reduce(fiveBucks.plus(tenFrans), "USD");
